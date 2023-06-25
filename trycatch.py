1cce82240e4a1392cbc554a24196da6b699b669c
Harshit Mishra <harshitlove28@outlook.com>
2023-06-26 01:28:44 +0530

    Progress can be tracked

--- trycatch.py
+++ trycatch.py
@@ -1,12 +1,26 @@
 import pyshark as ps
 import time
+
 start = time.time()
-cap=ps.FileCapture('data/http_tcp_80_traffic.pcap')
-i=1
-for packet in cap:
+cap = ps.FileCapture('upload/http_tcp_80_traffic.pcap', use_ek=True)
+# total_packets = len([packet for packet in cap])
+packets = []
+i = 1
+
+# print(len([packet for packet in cap]))
+cap.load_packets()
+packet_amount = len(cap)
+print(packet_amount)
+exit()
+
+for i, packet in enumerate(cap):
     # print(dir(packet))
     print(packet)
     break
+    # packets.append(packet)
+    # Calculate progress percentage
+    # progress = int((i + 1) / total_packets * 100)
+    # print("progress=> ",progress)
     # print(packet.tcp.options)
     # print(packet.tcp.field_names)
     # print(packet.tcp.option_len)
@@ -26,6 +40,7 @@ for packet in cap:
     #     print("Exception:", str(e))
     #     continue
 
-
 end = time.time()
 print(end - start)
+print(f'================ Total Length expected => {total_packets}')
+print(f'================= Actual Length => {len(packets)}')
